6d662d4af9b79326d534bf3cdd211c2d9b03fb62
Hairon&Young <yhl12388@126.com>
2017-08-19 21:37:09 +0800

    学习完【字符串和字符】

--- Swift3.0/字符串和字符/字符串和字符/main.swift
+++ Swift3.0/字符串和字符/字符串和字符/main.swift
@@ -156,10 +156,107 @@ for index in word.characters.indices {
 }
 
 
+//插入和删除
+welcome = "hello"
+welcome.insert("!", at: welcome.endIndex)
+print(welcome)
+
+welcome.insert(contentsOf: " there".characters, at: welcome.index(before: welcome.endIndex))
+print(welcome);
+
+welcome.remove(at: welcome.index(before: welcome.endIndex))
+print(welcome)
+
+let range = welcome.index(welcome.endIndex, offsetBy: -6)..<welcome.endIndex
+welcome.removeSubrange(range)
+print(welcome)
 
 
 
+//比较字符串
+let quotation = "We're a lot alike, you and I."
+let sameQuotation = "We're a lot alike, you and I."
+if quotation == sameQuotation {
+    print("These two strings are considered equal")
+}
 
+
+//使用LATIN SMALL LETTER E WITH ACUTE
+let eAcuteQuestion = "Voulez-vous un caf\u{E9}?"
+//使用 LATIN SMALL LETTER E and COMBINING ACUTE ACCENT
+let combinedEAcuteQuestion = "Voulez-vous un caf\u{65}\u{301}?"
+if eAcuteQuestion == combinedEAcuteQuestion {
+    print("These two strings are considered equal")
+}
+
+
+let latinCapitalLetterA: Character = "\u{41}"
+print(latinCapitalLetterA)
+
+let cyrillicCapitalLetterA: Character = "\u{0410}"
+print(cyrillicCapitalLetterA)
+
+if latinCapitalLetterA != cyrillicCapitalLetterA {
+    print("These two characters are not equal")
+}
+
+//前缀/后缀相等
+let romeoAndJuliet = [
+    "Act 1 Scene 1: Verona, A public place",
+    "Act 1 Scene 2: Capulet's mansion",
+    "Act 1 Scene 3: A room in Capulet's mansion",
+    "Act 1 Scene 4: A street outside Capulet's mansion",
+    "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+    "Act 2 Scene 1: Outside Capulet's mansion",
+    "Act 2 Scene 2: Capulet's orchard",
+    "Act 2 Scene 3: Outside Friar Lawrence's cell",
+    "Act 2 Scene 4: A street in Verona",
+    "Act 2 Scene 5: Capulet's mansion",
+    "Act 2 Scene 6: Friar Lawrence's cell"
+]
+
+var act1SceneCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasPrefix("Act 1") {
+        act1SceneCount += 1
+    }
+}
+print("There are \(act1SceneCount) scenes in Act 1")
+
+
+var mansionCount = 0
+var cellCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasSuffix("Capulet's mansion") {
+        mansionCount += 1
+    } else if scene.hasSuffix("Friar Lawrence's cell") {
+        cellCount += 1
+    }
+}
+print("\(mansionCount) mansion scenes;\(cellCount) cell scenes")
+
+
+
+//字符串的Unioncode表现形式
+let dogString = "Dog‼🐶"
+for codeUnit in dogString.utf8 {
+    print("\(codeUnit) ",terminator:"")
+}
+print("")
+
+for codeUnit in dogString.utf16 {
+    print("\(codeUnit) ",terminator:"")
+}
+print("")
+
+for scalar in dogString.unicodeScalars {
+    print("\(scalar.value) ",terminator:"")
+}
+print("")
+
+for scalar in dogString.unicodeScalars {
+    print("\(scalar) ")
+}
 
 
 
