76c0d38dee3d9c4e7f0c44df3051422ea6ff744e
yhl <1351268663@qq.com>
2017-08-22 20:21:08 +0800

    学习完【控制流】

--- Swift3.0/控制流/控制流/main.swift
+++ Swift3.0/控制流/控制流/main.swift
@@ -10,78 +10,421 @@ import Foundation
 
 //print("Hello, World!")
 
+//MARK:For-In循环
+let names = ["Anna", "Alex", "Brian", "Jack"]
+for name in names {
+    print(name, separator: "", terminator: " ")
+}
+print("")
 
-//For-In循环
+let numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+for (animalName, legCount) in numberOfLegs {
+    print("\(animalName)s have \(legCount) legs")
+}
 
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
 
+//使用下划线(_)替代变量名来忽略这个值
+let base = 3
+let power = 10
+var answer = 1
+for _ in 1...power {
+    answer *= base;
+}
+print("\(base) to the power of \(power) is \(answer)") // 3的10次幂
 
-//While循环
+let minutes = 60
+for tickMark in 0..<minutes {
+    // 每1分钟呈现一个刻度线（60次）
+}
 
+let minuteInterval = 5
+for tickMark in stride(from: 0, to: minutes, by: minuteInterval) {
+    // 每5分钟呈现一个刻度线 (0, 5, 10, 15 ... 45, 50, 55)
+}
 
-
-//蛇和梯子小游戏
-
-
-
-
-//条件语句(If/Switch)
-
-
-
-
-
-//不存在隐式的贯穿
-
-
-
-
-
-//区间匹配
-
-
-
-
-//元组
-
-
-
-
-//值绑定
-
-
-
-
-//Where
+let hours = 12
+let hourInterval = 3
+for tickMark in stride(from: 3, through: hours, by: hourInterval) {
+    // 每3小时呈现一个刻度线 (3, 6, 9, 12)
+}
 
 
 
-
-//复合匹配
-
-
-
-//控制转移语句(continue/break/fallthrough/return/throw)
-
+//MARK:While循环
+/*
+while 循环，每次在循环开始时计算条件是否符合；
+repeat-while 循环，每次在循环结束时计算条件是否符合。
+*/
 
 
+/*
+ * 蛇和梯子小游戏
+ * 游戏规则如下：
+ 
+ 游戏盘面包括 25 个方格，游戏目标是达到或者超过第 25 个方格；
+ 每一轮，你通过掷一个六面体骰子来确定你移动方块的步数，移动的路线由上图中横向的虚线所示；
+ 如果在某轮结束，你移动到了梯子的底部，可以顺着梯子爬上去；
+ 如果在某轮结束，你移动到了蛇的头部，你会顺着蛇的身体滑下去。
+ 
+ */
 
-//贯穿
+//方法一
+let finalSquare = 25
+var board = [Int].init(repeating: 0, count: finalSquare + 1)
+
+board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02;
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08;
+print("board.count = \(board.count)")
+
+var square = 0
+var diceRoll = 0
+while square < finalSquare {
+    // 掷骰子
+    diceRoll += 1
+    if diceRoll == 7 {
+        diceRoll = 1
+    }
+    // 根据点数移动
+    square += diceRoll
+    if square < board.count {
+        // 如果玩家还在棋盘上，顺着梯子爬上去或者顺着蛇滑下去
+        square += board[square]
+    }
+}
+print("Game over!")
 
 
-//带标签的语句
+//方法二
+square = 0
+diceRoll = 0
+repeat  {
+    // 顺着梯子爬上去或者顺着蛇滑下去
+    square += board[square]
+    // 掷骰子
+    diceRoll += 1
+    if diceRoll == 7 {
+        diceRoll = 1
+    }
+    // 根据点数移动
+    square += diceRoll
+} while square < finalSquare
+print("Game over!")
+
+
+
+
+//MARK:条件语句(If/Switch)
+var temperatureInFahrenheit = 30
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+}
+
+temperatureInFahrenheit = 40
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else {
+    print("It's not that code. Wear a t-shirt.")
+}
+
+temperatureInFahrenheit = 90
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else if temperatureInFahrenheit >= 86 {
+    print("It's really warm. Don't forget to wear sunscreen.")
+} else {
+    print("It's not that code. Wear a t-shirt.")
+}
+
+
+
+
+//MARK:Switch
+/*
+ switch语句必须是完备的。这就是说，每一个可能的值都必须至少有一个 case 分支与之对应。在某些不可能涵盖所有值的情况下，你可以使用默认（default）分支来涵盖其它所有没有对应的值，这个默认分支必须在switch语句的最后面。
+ */
+
+var someCharacter: Character = "z"
+switch someCharacter {
+case "a":
+    print("The first letter of the alphabet")
+case "z":
+    print("The last letter of the alphabet")
+default:
+    print("Some other character")
+}
+
+
+
+//MARK:不存在隐式的贯穿
+/*
+ 与 C 和 Objective-C 中的switch语句不同，在 Swift 中，当匹配的 case 分支中的代码执行完毕后，程序会终止switch语句，而不会继续执行下一个 case 分支。这也就是说，不需要在 case 分支中显式地使用break语句。这使得switch语句更安全、更易用，也避免了因忘记写break语句而产生的错误。
+ /*
+ 注意： 虽然在Swift中break不是必须的，但你依然可以在 case 分支中的代码执行完毕前使用break跳出，详情请参见Switch 语句中的 break。
+ */
+ 
+ */
+
+let anotherCharacter: Character = "a"
+switch anotherCharacter {
+case "a", "A":
+    print("The letter A")
+default:
+    print("Not the letter A")
+}
+
+
+
+//MARK:区间匹配
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+var naturalCount: String
+switch approximateCount {
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "several"
+case 12..<100:
+    naturalCount = "dozens of"
+case 100..<1000:
+    naturalCount = "hundreds of"
+default:
+    naturalCount = "many"
+}
+print("There are \(naturalCount) \(countedThings).")
+
+
+
+
+//MARK:元组
+//元组中的元素可以是值，也可以是区间。另外，使用下划线（_）来匹配所有可能的值。
+
+let somePoint = (1, 1)
+switch somePoint {
+case (0, 0):
+    print("(0, 0) is at the origin")
+case (_, 0):
+    print("(\(somePoint.0), 0) is on the x-axis")
+case (0, _):
+    print("(0, \(somePoint.1)) is on the y-axis")
+case (-2...2, -2...2):
+    print("(\(somePoint.0), \(somePoint.1)) is inside the box")
+default:
+    print("(\(somePoint.0), \(somePoint.1)) is outside of the box")
+}
+
+
+
+//MARK:值绑定
+let anotherPoint = (2, 0)
+switch anotherPoint {
+case (let x, 0):
+    print("on the x-axis with an x value of \(x)")
+case (0, let y):
+    print("on the y-axis with a y value of \(y)")
+case (let x, let y):
+    print("somewhere else at (\(x), \(y))")
+}
+
+
+
+//MARK:Where
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+case let (x, y) where x == y:
+    print("(\(x), \(y)) is on the line x == y")
+case let (x, y) where x == -y:
+    print("(\(x), \(y)) is on the line x == -y")
+case let (x, y):
+    print("(\(x), \(y)) is just some arbitrary point")
+}
+
+
+
+//MARK:复合匹配
+/*
+当多个条件可以使用同一种方法来处理时，可以将这几种可能放在同一个case后面，并且用逗号隔开。当case后面的任意一种模式匹配的时候，这条分支就会被匹配。并且，如果匹配列表过长，还可以分行书写
+*/
+
+someCharacter = "e"
+switch someCharacter {
+case "a", "e", "i", "o", "u":
+    print("\(someCharacter) is a vowel")
+case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+     "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
+    print("\(someCharacter) is a consonant")
+default:
+    print("\(someCharacter) is not a vowel or a consonant")
+}
+
+let stillAnotherPoint = (9, 0)
+switch stillAnotherPoint {
+case (let distance, 0), (0, let distance):
+    print("On an axis, \(distance) from the origin")
+default:
+    print("Not on an axis")
+}
+
+
+
+//MARK:控制转移语句(continue/break/fallthrough/return/throw)
+/*
+continue
+break
+fallthrough
+return
+throw
+*/
+
+//continue语句告诉一个循环体立刻停止本次循环，重新开始下次循环。就好像在说“本次循环我已经执行完了”，但是并不会离开整个循环体。
+let puzzleInput = "great minds think alike"
+var puzzleOutput = ""
+for character in puzzleInput.characters {
+    switch character {
+    case "a", "e", "i", "o", "u", " ":
+        continue
+    default:
+        puzzleOutput.append(character)
+    }
+}
+print(puzzleOutput)
+
+
+//break语句会立刻结束整个控制流的执行。当你想要更早的结束一个switch代码块或者一个循环体时，你都可以使用break语句。
+/*
+循环语句中的 break
+
+当在一个循环体中使用break时，会立刻中断该循环体的执行，然后跳转到表示循环体结束的大括号(})后的第一行代码。不会再有本次循环的代码被执行，也不会再有下次的循环产生。
+
+
+Switch 语句中的 break
+
+当在一个switch代码块中使用break时，会立即中断该switch代码块的执行，并且跳转到表示switch代码块结束的大括号(})后的第一行代码。
+
+ /*
+ 注意： 当一个switch分支仅仅包含注释时，会被报编译时错误。注释不是代码语句而且也不能让switch分支达到被忽略的效果。你应该使用break来忽略某个分支。
+ */
+
+*/
+
+let numberSymbol: Character = "三"
+var possibleIntegerValue: Int?
+switch numberSymbol {
+case "1", "١", "一", "๑":
+    possibleIntegerValue = 1
+case "2", "٢", "二", "๒":
+    possibleIntegerValue = 2
+case "3", "٣", "三", "๓":
+    possibleIntegerValue = 3
+case "4", "٤", "四", "๔":
+    possibleIntegerValue = 4
+default:
+    break
+}
+
+if let integerValue = possibleIntegerValue {
+    print("The integer of \(numberSymbol) is \(integerValue).")
+} else {
+    print("An integer value could not be fount for \(numberSymbol).")
+}
+
+
+
+
+//MARK:贯穿
+/*
+Swift 中的switch不会从上一个 case 分支落入到下一个 case 分支中。相反，只要第一个匹配到的 case 分支完成了它需要执行的语句，整个switch代码块完成了它的执行。相比之下，C 语言要求你显式地插入break语句到每个 case 分支的末尾来阻止自动落入到下一个 case 分支中。Swift 的这种避免默认落入到下一个分支中的特性意味着它的switch 功能要比 C 语言的更加清晰和可预测，可以避免无意识地执行多个 case 分支从而引发的错误。
+*/
+
+let integerToDescribe = 5
+var description = "The number \(integerToDescribe) is"
+switch integerToDescribe {
+case 2, 3, 5, 7, 11, 13, 17, 19:
+    description += " a prime number, and also"
+    fallthrough
+default:
+    description += " an integer."
+}
+print(description)
+
+
+
+
+//MARK:带标签的语句
+/*
+在 Swift 中，你可以在循环体和条件语句中嵌套循环体和条件语句来创造复杂的控制流结构。并且，循环体和条件语句都可以使用break语句来提前结束整个代码块。因此，显式地指明break语句想要终止的是哪个循环体或者条件语句，会很有用。类似地，如果你有许多嵌套的循环体，显式指明continue语句想要影响哪一个循环体也会非常有用。
+
+为了实现这个目的，你可以使用标签（statement label）来标记一个循环体或者条件语句，对于一个条件语句，你可以使用break加标签的方式，来结束这个被标记的语句。对于一个循环语句，你可以使用break或者continue加标签，来结束或者继续这条被标记语句的执行。
+
+声明一个带标签的语句是通过在该语句的关键词的同一行前面放置一个标签，作为这个语句的前导关键字(introducor keyword)，并且该标签后面跟随一个冒号。下面是一个针对while循环体的标签语法，同样的规则适用于所有的循环体和条件语句。
+
+    label name: while condition { statements }
+*/
+
+
+//【蛇和梯子】的游戏增加一条规则：为了获胜，你必须刚好落在第 25 个方块中。
+square = 0
+diceRoll = 0
+gameLoop: while square != finalSquare {
+    diceRoll += 1
+    if diceRoll == 7 { diceRoll = 1 }
+    switch square + diceRoll {
+    case finalSquare:
+        // 骰子数刚好使玩家移动到最终的方格里，游戏结束。
+        break gameLoop
+    case let newSquare where newSquare > finalSquare:
+        // 骰子数将会使玩家的移动超出最后的方格，那么这种移动是不合法的，玩家需要重新掷骰子
+        continue gameLoop
+    default:
+        // 合法移动，做正常的处理
+        square += diceRoll
+        square += board[square]
+    }
+}
+print("Game over!")
 
 
 
 
 
-//提前退出
+//MARK:提前退出
+/*
+像if语句一样，guard的执行取决于一个表达式的布尔值。我们可以使用guard语句来要求条件必须为真时，以执行guard语句后的代码。不同于if语句，一个guard语句总是有一个else从句，如果条件不为真则执行else从句中的代码。
+*/
+
+func greet(person: [String: String]) {
+    guard let name = person["name"] else {
+        return
+    }
+    print("Hello \(name)")
+    
+    guard let location = person["location"] else {
+        print("I hope the weather is nice near you.")
+        return
+    }
+    print("I hope the weather is nice in \(location).")
+}
+greet(person: ["name": "John"])
+greet(person: ["name": "John", "location": "Cupertino"])
+/*
+ 相比于可以实现同样功能的if语句，按需使用guard语句会提升我们代码的可读性。它可以使你的代码连贯的被执行而不需要将它包在else块中，它可以使你在紧邻条件判断的地方，处理违规的情况。
+ */
 
 
 
 
+//MARK:检测API可用性
+//Swift内置支持检查 API 可用性，这可以确保我们不会在当前部署机器上，不小心地使用了不可用的API。
 
-//检测API可用性
-
+if #available(iOS 10, macOS 10.12, *) {
+    // 在 iOS 使用 iOS 10 的 API, 在 macOS 使用 macOS 10.12 的 API
+} else {
+    // 使用先前版本的 iOS 和 macOS 的 API
+}
 
 
 
